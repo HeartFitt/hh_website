a14201854a8ac900886589f0b1c8edcf1f4cd943
Max Lair <max@lairfamily.com>
2025-07-17 13:26:44 -0400

    Condensed Hero

--- app/pages/welcome.tsx
+++ app/pages/welcome.tsx
@@ -1,10 +1,85 @@
 import React from "react";
-import Hero from "~/components/Hero";
+import { SneakerMoveIcon, CheckIcon, BuildingsIcon, type IconProps } from "@phosphor-icons/react";
+import ProductCard from "~/components/ProductCard";
+import Button from "~/hkit/Button";
+
+type ProductCardContent = {
+    title: string;
+    actionButton: React.ReactNode;
+    icon?: React.ElementType<IconProps>;
+    iconColor?: string;
+    desc?: string;
+    features?: string[];
+}
+
+const cardContent: ProductCardContent[] = [
+    {
+        title: "Personal",
+        actionButton: <Button label="Get Started" size="lg" variant="primary" fillWidth={true} />,
+        icon: SneakerMoveIcon,
+        iconColor: "var(--orange-500)",
+        desc: "Make exercise fun & rise on the leaderboard.",
+        features: [
+            "App calibrated to personal hardware",
+            "Let’s you exercise at your pace"
+        ]
+    },
+    {
+        title: "Business",
+        actionButton: <Button label="Join Now" size="lg" variant="secondary" fillWidth={true} />,
+        icon: BuildingsIcon,
+        iconColor: "var(--green-500)",
+        desc: "Turn cardio into income.",
+        features: [
+            "Club leaderboards",
+            "Heart rate detection hardware",
+            "Custom theme tailored to your club"
+        ]
+    }
+]
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-        <Hero />
+      {/* Hero */}
+      <section>
+        {/* Wrapper */}
+        <div className='flex flex-col items-center gap-[8rem]'>
+          {/* Title */}
+          <div className="w-full align-center text-center">
+            <h1 className='title-1'>
+              Gamified,
+              <br />
+              Heart-healthy <br />
+              Exercise.
+            </h1>
+          </div>
+          {/* Cards */}
+          <div className='flex gap-4'>
+            {cardContent.map((card, index) => (
+              <ProductCard
+                key={index}
+                title={card.title}
+                actionButton={card.actionButton}
+                icon={card.icon}
+                iconColor={card.iconColor}
+              >
+                <div></div>
+                <p className="text-md text-neutral-200">{card.desc}</p>
+                <ol >
+                  {card.features?.map((feature, index) => (
+                    <li className="flex flex-row gap-2 items-center" key={index}>
+                      <CheckIcon size={16} weight="bold" color="var(--neutral-500)" />
+                      <span className="font-normal text-md text-neutral-200">{feature}</span>
+                    </li>
+                  ))}
+
+                </ol>
+              </ProductCard>
+            ))}
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
